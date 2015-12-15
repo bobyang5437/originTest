1e2ed626017df61b7ee27ef6c09c3de10eb0ef68
yang <yang@yangdemac-mini.local>
2015-12-15 11:46:42 +0800

    更改

--- swiftDemo/swiftDemo/ViewController.swift
+++ swiftDemo/swiftDemo/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.groupTableViewBackgroundColor()
+        
+        label = UILabel()
+        label.frame = CGRectMake(50, 50, 100, 100)
+        label.backgroundColor = UIColor.redColor()
+        label.text = "this is test"
+        label.textAlignment = .Center
+        self.view.addSubview(label)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
